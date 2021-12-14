07e49e9872a2fb8ea8f5f8898820db4204eaf06d
Sanity.io <hello@sanity.io>
2021-12-14 15:40:52 -0500

    Initial commit

---
+++ studio/dashboardConfig.js
@@ -0,0 +1,52 @@
+export default {
+  widgets: [
+    {
+      name: 'sanity-tutorials',
+      options: {
+        templateRepoId: 'sanity-io/sanity-template-gatsby-blog'
+      }
+    },
+    {
+      name: 'project-info',
+      options: {
+        __experimental_before: [
+          {
+            name: 'netlify',
+            options: {
+              description:
+                'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
+              sites: [
+                {
+                  buildHookId: '',
+                  title: 'Sanity Studio',
+                  name: 'sanity-svelte-kit-studio',
+                  apiId: '56db15c0-5414-4b8d-ba53-9054e9b02891'
+                },
+                {
+                  buildHookId: '',
+                  title: 'Blog Website',
+                  name: '',
+                  apiId: ''
+                }
+              ]
+            }
+          }
+        ],
+        data: [
+          {
+            title: 'GitHub repo',
+            value: 'https://github.com/juanmesa2097/sanity-svelte-kit',
+            category: 'Code'
+          },
+          { title: 'Frontend', value: '', category: 'apps' }
+        ]
+      }
+    },
+    { name: 'project-users', layout: { height: 'auto' } },
+    {
+      name: 'document-list',
+      options: { title: 'Recent blog posts', order: '_createdAt desc', types: ['post'] },
+      layout: { width: 'medium' }
+    }
+  ]
+}
